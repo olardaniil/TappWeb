9fe2f3b6a5bfc48b22b0f1499e9059a3906e84ae
olardaniil <123248287+olardaniil@users.noreply.github.com>
2023-05-23 14:48:20 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -44,9 +44,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a3cc984817bebffbc5f1aa4bc0d637f9",
-"/": "a3cc984817bebffbc5f1aa4bc0d637f9",
-"main.dart.js": "f10ae3c15cbc11bfa0209fd1a1db349a",
+"index.html": "d96735518eab41a69b2ca2f2271bd441",
+"/": "d96735518eab41a69b2ca2f2271bd441",
+"main.dart.js": "f4f01cdfcdb6b25657b47b24881e809c",
 "manifest.json": "330c93be84fe52710e7e4457f42e5412",
 "version.json": "3b5c044b04b8f2359a40059906b278de"
 };
